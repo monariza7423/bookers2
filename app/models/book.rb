23114d19a2f9dfc1efb86480e0dc22bf77353f17
EC2 Default User <ec2-user@ip-172-31-5-225.ec2.internal>
2022-06-27 12:52:28 +0000

    6/27

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ApplicationRecord
+  has_one_atached :image
   beongs_to :user
+
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit:[width,height]).processed
+  end
 end
