434ae09da168dab2b0b250a82737fe1c7de3f35a
EC2 Default User <ec2-user@ip-172-31-5-225.ec2.internal>
2022-07-05 13:19:40 +0000

    7/5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-   before_action :authenticate_user!, except: [:top]
+   before_action :authenticate_user!, except: [:top, :about]
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
     end
 end
